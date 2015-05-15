2187543379f4be909e5f6a4e25db35fe2f8fa22c
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 19:34:57 +0100

    Separate gem specification

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'rake'
+
+PKG_NAME = 'hornetseye-narray'
+PKG_VERSION = '1.0.2'
+RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+BIN_FILES = [ 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+SUMMARY = %q{NArray integration for Hornetseye}
+DESCRIPTION = %q{This Ruby extension implements conversions from Hornetseye::MultiArray to NArray and vice versa.}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-narray/}
+
+
